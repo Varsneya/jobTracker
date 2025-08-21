8821bf342ee2ff956804b961c5632ca1ef696335
Varsneya Srinivas <vsrinivas@adobe.com>
2025-08-21 16:31:02 +0530

    inital commit

---
+++ src/test/java/com/adobe/guides/jobTracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adobe.guides.jobTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
